5f3afe4ee701c2a45c85c80ee8bc19fab7683b82
jelennal <jelena.luketina@aalto.fi>
2015-02-04 23:57:22 +0200

    Delete FindPCA.m

--- FindPCA.m
+++
@@ -1,7 +0,0 @@
-function [coeffMatrix, dataProjection] = FindPCA(input)
-
-% ------------------------------------------ specifying PCA
-[coeffMatrix, dataProjection, latent] = pca(input,'NumComponents', 2);
-
-
-end
